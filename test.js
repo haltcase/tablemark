b29fb542068d31e59ef5a4a92c2d193509c806a7
Thomas Jensen <thomas@src.agency>
2017-09-03 11:43:02 +0200

    Refactor
    - `let` to `const`
    - `() => { return ... }` to `() => ...`
    - More descriptive variable names

--- test.js
+++ test.js
@@ -4,22 +4,22 @@ import fn from './'
 import cases from './fixtures/inputs'
 
 test('outputs the expected markdown', t => {
-  let result = fn(cases.standard.input)
+  const result = fn(cases.standard.input)
   t.is(result, cases.standard.expected)
 })
 
 test('works when provided alignment options', t => {
-  let result = fn(cases.alignments.input, cases.alignments.options)
+  const result = fn(cases.alignments.input, cases.alignments.options)
   t.is(result, cases.alignments.expected)
 })
 
 test('replaces column names when provided', t => {
-  let result = fn(cases.columns.input, cases.columns.options)
+  const result = fn(cases.columns.input, cases.columns.options)
   t.is(result, cases.columns.expected)
 })
 
 test('can override sentence casing', t => {
-  let result = fn(cases.casing.input, cases.casing.options)
+  const result = fn(cases.casing.input, cases.casing.options)
   t.is(result, cases.casing.expected)
 })
 
