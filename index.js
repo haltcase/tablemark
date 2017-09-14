9bbea6c532985a27f455ba58d8974374af11b0d0
Bo Lingen <lingenbw@gmail.com>
2017-09-14 00:05:44 +0000

    style: let -> const and misc other nits

--- index.js
+++ index.js
@@ -3,34 +3,30 @@
 const os = require('os')
 const sentence = require('sentence-case')
 
-const ALIGN = [ 'LEFT', 'CENTER', 'RIGHT' ]
-
 const columnWidthMin = 5
+const ALIGN = [ 'LEFT', 'CENTER', 'RIGHT' ]
 
 module.exports = (input, options) => {
   if (!Array.isArray(input)) {
-    throw new TypeError(
-      `Expected an Array, got ${typeof input}`
-    )
+    throw new TypeError(`Expected an Array, got ${typeof input}`)
   }
 
   options = Object.assign({
     stringify: v => typeof v === 'undefined' ? '' : String(v)
   }, options)
 
-  let stringify = options.stringify
+  const stringify = options.stringify
+  const keys = Object.keys(input[0])
 
   let table = ''
 
-  let keys = Object.keys(input[0])
-
-  let titles = keys.map((key, i) => {
+  const titles = keys.map((key, i) => {
     if (Array.isArray(options.columns) && options.columns[i]) {
-        if (typeof options.columns[i] === 'string') {
-          return options.columns[i]
-        } else if (options.columns[i].name) {
-          return options.columns[i].name
-        }
+      if (typeof options.columns[i] === 'string') {
+        return options.columns[i]
+      } else if (options.columns[i].name) {
+        return options.columns[i].name
+      }
     }
 
     if (options.caseHeaders === false) return key
@@ -38,19 +34,20 @@ module.exports = (input, options) => {
     return sentence(key)
   })
 
-  let widths = input.reduce(
+  const widths = input.reduce(
     (sizes, item) => keys.map(
       (key, i) => Math.max(columnWidthMin, stringify(item[key]).length, sizes[i])
     ),
     titles.map(t => t.length)
   )
 
-  let alignments = keys.map((key, i) => {
-    if (Array.isArray(options.columns)
-      && options.columns[i]
-      && options.columns[i].align
+  const alignments = keys.map((key, i) => {
+    if (
+      Array.isArray(options.columns) &&
+      options.columns[i] &&
+      options.columns[i].align
     ) {
-      let align = String(options.columns[i].align).toUpperCase()
+      const align = String(options.columns[i].align).toUpperCase()
 
       if (ALIGN.indexOf(align) === -1) {
         throw new TypeError(`Unknown alignment, got ${options.columns[i].align}`)
@@ -67,9 +64,11 @@ module.exports = (input, options) => {
 
   // header separator
   table += row(alignments.map((align, i) => {
-    return (align === 'LEFT' || align === 'CENTER' ? ':' : '-')
-      + repeat('-', widths[i] - 2)
-      + (align === 'RIGHT' || align === 'CENTER' ? ':' : '-')
+    return (
+      (align === 'LEFT' || align === 'CENTER' ? ':' : '-') +
+      repeat('-', widths[i] - 2) +
+      (align === 'RIGHT' || align === 'CENTER' ? ':' : '-')
+    )
   }))
 
   // table body
@@ -82,7 +81,7 @@ module.exports = (input, options) => {
   return table
 }
 
-function pad(alignment, target, value){
+function pad (alignment, target, value) {
   if (!alignment || alignment === 'LEFT') {
     return padEnd(value, target)
   }
@@ -92,13 +91,13 @@ function pad(alignment, target, value){
   }
 
   // CENTER
-  let remainder = (target - value.length) % 2
-  let sides = (target - value.length - remainder) / 2
+  const remainder = (target - value.length) % 2
+  const sides = (target - value.length - remainder) / 2
 
   return repeat(' ', sides) + value + repeat(' ', sides + remainder)
 }
 
-function row(v){
+function row (v) {
   if (Array.isArray(v)) {
     v = v.join(' | ')
   }
@@ -106,14 +105,14 @@ function row(v){
   return '| ' + v + ' |' + os.EOL
 }
 
-function repeat(what, times){
+function repeat (what, times) {
   return new Array(times).fill(what).join('')
 }
 
-function padStart(what, target, start){
+function padStart (what, target, start) {
   return repeat(' ', target - what.length) + what
 }
 
-function padEnd(what, target, start){
+function padEnd (what, target, start) {
   return what + repeat(' ', target - what.length)
 }
