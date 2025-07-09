31755aa7f460e1f3234d11f9ff12224f62981fdf
haltcase <bo@haltcase.dev>
2025-07-08 23:06:54 -0500

    test: increase timeout for big data tests

--- tests/big-data.test.ts
+++ tests/big-data.test.ts
@@ -3,16 +3,22 @@ import { describe, expect, test } from "vitest";
 import { tablemark } from "../src/index.js";
 import data5mb from "./fixtures/5MB.json" with { type: "json" };
 
-describe("big data", () => {
-	test("5MB.json", () =>
-		expect(tablemark(data5mb as Record<string, unknown>[])).toMatchFileSnapshot(
-			"./__snapshots__/5MB.json.md"
-		));
+const timeout = 20_000;
 
-	test("5MB.json when `textHandlingStrategy` is `basic`", () =>
-		expect(
-			tablemark(data5mb as Record<string, unknown>[], {
-				textHandlingStrategy: "basic"
-			})
-		).toMatchFileSnapshot("./__snapshots__/5MB.json-basic.md"));
-});
+describe(
+	"big data",
+	() => {
+		test("5MB.json", () =>
+			expect(
+				tablemark(data5mb as Record<string, unknown>[])
+			).toMatchFileSnapshot("./__snapshots__/5MB.json.md"));
+
+		test("5MB.json when `textHandlingStrategy` is `basic`", () =>
+			expect(
+				tablemark(data5mb as Record<string, unknown>[], {
+					textHandlingStrategy: "basic"
+				})
+			).toMatchFileSnapshot("./__snapshots__/5MB.json-basic.md"));
+	},
+	timeout
+);
