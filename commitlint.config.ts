fb2ba6ba84e29bbcc54b680b4d1936ffc58044fe
haltcase <bo@haltcase.dev>
2025-07-08 22:33:38 -0500

    chore: set up commitlint

---
+++ commitlint.config.ts
@@ -0,0 +1,40 @@
+import type { UserConfig } from "@commitlint/types";
+
+export default {
+	extends: ["@commitlint/config-conventional"],
+	// remove this if we can ever get dependabot to follow the rules
+	// https://github.com/dependabot/dependabot-core/issues/2445
+	ignores: [(message) => message.includes("Signed-off-by: dependabot[bot]")],
+	rules: {
+		"type-enum": [
+			2,
+			"always",
+			[
+				// Change that affects the build system or dependencies
+				"build",
+				// Change to project metadata or tooling
+				"chore",
+				// Change to CI workflows
+				"ci",
+				// Change that only affects documentation
+				"docs",
+				// New feature
+				"feat",
+				// Bug fix
+				"fix",
+				// Code change that improves performance
+				"perf",
+				// Code change that neither fixes a bug nor adds a feature
+				"refactor",
+				// Commit that reverts a previous commit
+				"revert",
+				// Change that does not affect the meaning of the code
+				"style",
+				// Addition of missing tests or corrections for existing tests
+				"test",
+				// For automated releases only
+				"release"
+			]
+		]
+	}
+} satisfies UserConfig;
