cd3fcc9ca830eedd73662e892b789584df10e96a
Bo Lingen <bo@haltcase.dev>
2025-08-28 21:00:17 -0500

    Add failing test for issue 24 (empty cells) (#25)

---
+++ tests/issues/24.test.ts
@@ -0,0 +1,22 @@
+// https://github.com/haltcase/tablemark/issues/24
+
+import { expect, test } from "vitest";
+
+import { tablemark } from "../../src/index.js";
+import { snapshotFile } from "../helpers.js";
+
+test("wraps empty cells as expected with `textHandlingStrategy: 'basic'`", async () => {
+	const data = [
+		{
+			name: "Bob",
+			age: 25,
+			city: undefined
+		}
+	];
+
+	await expect(
+		tablemark(data, {
+			textHandlingStrategy: "basic"
+		})
+	).toMatchFileSnapshot(snapshotFile("issues/24-empty-cells"));
+});
